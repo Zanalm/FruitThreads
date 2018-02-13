5d9dc2976a38c1c08bfb0e17bcb3b51139baba89
Zander <31848787+Zanalm@users.noreply.github.com>
2018-02-13 12:09:32 +0100

    test
    test

---
+++ src/GruThreadFactory.java
@@ -0,0 +1,32 @@
+import java.util.Random;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.atomic.AtomicInteger;
+
+
+public class GruThreadFactory implements ThreadFactory {
+    
+    static final AtomicInteger poolNumber = new AtomicInteger(1);
+    final ThreadGroup group;
+    final AtomicInteger threadNumber = new AtomicInteger(1);
+    final int namePrefix;
+    final String[] persons;
+    
+    public GruThreadFactory(String[] persons) {
+        SecurityManager s = System.getSecurityManager();
+        group = (s != null) ? s.getThreadGroup() : Thread.currentThread().getThreadGroup();
+        namePrefix =  poolNumber.getAndIncrement();
+        this.persons = persons; 
+    }
+
+    @Override
+    public Thread newThread(Runnable r) {
+        int index = new Random().nextInt(persons.length);
+        final Thread t = new Thread(group, r, namePrefix + persons[index], 0);
+        if (t.isDaemon())
+            t.setDaemon(false);
+        if (t.getPriority() != Thread.NORM_PRIORITY)
+            t.setPriority(Thread.NORM_PRIORITY);
+        return t;
+    }
+
+}
