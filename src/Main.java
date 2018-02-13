f3d3a3a14f5b3304cf4d5174f3d3286829e92a9a
Zander <31848787+Zanalm@users.noreply.github.com>
2018-02-13 12:39:34 +0100

    test2
    test2

--- src/Main.java
+++ src/Main.java
@@ -3,36 +3,34 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadFactory;
 
-
 // the site with this example http://blog.weetech.co/2014/11/how-to-set-java-thread-name-thread-pool.html
 public class Main {
 
+	public static void main(String[] args) throws InterruptedException {
+		ThreadFactory GruFactory = null;
+		ExecutorService exec = null;
 
-    public static void main(String[] args) throws InterruptedException {
-        ThreadFactory GruFactory = null;
-        ExecutorService exec = null;
-        
-        try {
-            String[] Persons = {"Dave", "Stuart", "Kevin", "Jerry", "Carl", "Phil", "Paul", "Mike", "Jorge"};
-            GruFactory = new GruThreadFactory(Persons);
-            exec = Executors.newFixedThreadPool(Persons.length, GruFactory);
-            
-            for (int i = 0; i < 60; i++) {
-            	Person m = new Person();
-                exec.execute(m);
-            }
-            exec.shutdown();
-            while (!exec.isTerminated()) {
-                
-            }
-            
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            
-            System.out.println("done");
-        }
+		try {
+			String[] Persons = { "Dave", "Stuart", "Kevin", "Jerry", "Carl", "Phil", "Paul", "Mike", "Jorge" };
+			GruFactory = new GruThreadFactory(Persons);
+			exec = Executors.newFixedThreadPool(Persons.length, GruFactory);
 
-    }
+			for (int i = 0; i < 3; i++) { // 3 persons from the person-string
+				Person m = new Person();
+				exec.execute(m);
+			}
+			exec.shutdown();
+			while (!exec.isTerminated()) {
+
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+
+			System.out.println("done");
+		}
+
+	}
 
 }
--- src/Person.java
+++ src/Person.java
@@ -1,26 +1,25 @@
 import java.util.Random;
 
-
 public class Person implements Runnable {
-    
-    private static String[] fruits = {"apple", "banana", "Pear"}; 
-    
-    public Person() {
-     
-    }
 
-    @Override
-    public void run() {
-        try {
-            // banana or apple
-            int idx = new Random().nextInt(fruits.length);
-            System.out.printf("Person %s eating %s %n", Thread.currentThread().getName(), fruits[idx]);
-            Thread.sleep((idx * 1000) + 10000);
-            System.out.printf("Person %s done eat %s %n", Thread.currentThread().getName(), fruits[idx]);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+	private static String[] fruits = { "apple", "banana", "Pear" };
 
-    }
+	public Person() {
+
+	}
+
+	@Override
+	public void run() {
+		try {
+			// banana or apple
+			int idx = new Random().nextInt(fruits.length);
+			System.out.printf("Person %s eating %s %n", Thread.currentThread().getName(), fruits[idx]);
+			Thread.sleep((idx * 1000) + 3000);
+			System.out.printf("Person %s done eat %s %n", Thread.currentThread().getName(), fruits[idx]);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+	}
 
 }
